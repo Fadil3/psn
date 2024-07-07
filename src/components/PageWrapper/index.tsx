f584d0dbeda8c437955ccd7ba3e5677b3bf3742c
Fadil <mrayhanfadil@upi.edu>
2024-07-07 22:11:22 +0700

    feat(logout): add logout if user exist

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -1,22 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Button from "../Button";
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<{
+    username: string;
+  } | null>(null);
+
 
   useEffect(() => {
     const user = localStorage.getItem('user');
+    setUser(user ? JSON.parse(user) : null);
     const path = window.location.pathname;
-    console.log('path', path);
     if (!user) {
       if (path !== '/')
         window.location.href = '/';
     } else {
-      window.location.href = '/dashboard';
+      if (path === '/')
+        window.location.href = '/dashboard';
     }
   }, []);
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    window.location.href = '/';
+  }
+
   return (
-    <div className="container mx-auto px-4">
+    <main className="container mx-auto p-4">
+      {user &&
+        <header className="mb-4">
+          <nav className="flex items-center justify-between">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <div>
+              <span className="mr-4">{user?.username}</span>
+              <Button color="bg-red-500" onClick={logout}>Logout</Button>
+            </div>
+          </nav>
+        </header>
+      }
       {children}
-    </div>
+    </main>
   );
 }
 
